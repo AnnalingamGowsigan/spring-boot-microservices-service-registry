a5616808831e8947569b11dd8eb114d983a87f14
AnnalingamGowsigan <e18115@eng.pdn.ac.lk>
2024-03-15 14:34:48 +0530

    Add Eureka server configuration

---
+++ src/test/java/com/spring/springbootmicroservicesserviceregistry/SpringBootMicroservicesServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootmicroservicesserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
